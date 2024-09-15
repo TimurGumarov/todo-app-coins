4700fd08a1df733df3baf14d9dee9c5767e1fa76
Timur Gumarov <29134829+TimurGumarov@users.noreply.github.com>
2024-09-15 02:46:32 +0200

    Added simple EditPage logic and markup

--- src/pages/EditPage.vue
+++ src/pages/EditPage.vue
@@ -1,3 +1,35 @@
+<script setup lang="ts">
+import { store } from "../store"
+
+const props = defineProps({
+	id: Number,
+})
+
+const currentNote = store.notes.filter((note) => note.id === props.id)?.pop()
+</script>
+
 <template>
-	<h1>Edit</h1>
+	<div class="layout" v-if="currentNote">
+		<h1>{{ currentNote.title }}</h1>
+		<div class="note" @click="$emit('exitEditing')">
+			<ul v-if="currentNote.tasks?.length">
+				<li v-for="task of currentNote.tasks" :key="task.id">
+					{{ task.id }} {{ task.text }}
+				</li>
+			</ul>
+		</div>
+	</div>
 </template>
+
+<style scoped>
+.layout {
+	display: flex;
+	flex-flow: column;
+	gap: 20px;
+}
+.note {
+	display: flex;
+	flex-flow: column;
+	gap: 10px;
+}
+</style>
