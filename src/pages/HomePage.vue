828ba466738197248f79ae1e37527f6be50922b7
Timur Gumarov <29134829+TimurGumarov@users.noreply.github.com>
2024-09-15 02:43:59 +0200

    Refactored HomePage, added NotePreview component

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,24 +1,23 @@
 <script setup lang="ts">
+import NotePreview from "../components/NotePreview.vue"
 import { store } from "../store"
-
-// This sets the number of tasks to preview for each note
-const tasksPreviewCount = 2
 </script>
 
 <template>
-	<h1>Home</h1>
-	<div>
-		<div v-for="note of store.notes" :key="note.id">
-			<h3>{{ note.title }}</h3>
-			<ul v-if="note.tasks?.length">
-				<li
-					v-for="task of note.tasks.slice(0, tasksPreviewCount)"
-					:key="task.id"
-				>
-					{{ task.text }}
-				</li>
-				<li v-if="note.tasks.length > tasksPreviewCount">…</li>
-			</ul>
-		</div>
+	<div class="layout">
+		<h1>Заметки</h1>
+		<NotePreview
+			v-for="note of store.notes"
+			:note="note"
+			@click="$emit('enterEditing', note.id)"
+		/>
 	</div>
 </template>
+
+<style scoped>
+.layout {
+	display: flex;
+	flex-flow: column;
+	gap: 20px;
+}
+</style>
