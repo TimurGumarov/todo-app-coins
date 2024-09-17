6ccaa986f040a120c42706482515cb3e5d5cb18f
Timur Gumarov <29134829+TimurGumarov@users.noreply.github.com>
2024-09-17 23:05:06 +0200

    Note add button added + styles changes

--- src/components/NotePreview.vue
+++ src/components/NotePreview.vue
@@ -30,12 +30,20 @@ div {
 	display: flex;
 	flex-flow: column;
 	gap: 10px;
-	border: 1px solid #333;
 	border-radius: 10px;
+	background: linear-gradient(97deg, #202a39 -8.29%, #151d29 70.65%);
+	box-shadow: rgba(18, 18, 18, 0.25) 0px 0 0 0, rgba(0, 0, 0, 0.3) 0px 0 0 0;
 	margin: 0;
 	padding: 20px;
 	cursor: pointer;
+	transition: box-shadow 0.2s ease-in-out;
 }
+
+div:hover {
+	box-shadow: rgba(18, 18, 18, 0.25) 0px 5px 20px -1px,
+		rgba(0, 0, 0, 0.3) 0px 3px 3px -1px;
+}
+
 h3 {
 	margin: 0;
 }
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import NotePreview from "../components/NotePreview.vue"
 import { store } from "../store"
+import IconAdd from "../assets/icon-add.vue"
 </script>
 
 <template>
@@ -11,6 +12,9 @@ import { store } from "../store"
 			:note="note"
 			@click="$emit('enterEditing', note.id)"
 		/>
+		<button class="add-note">
+			<IconAdd :style="{ fill: 'rgb(0 154 92)' }" />
+		</button>
 	</div>
 </template>
 
@@ -19,5 +23,22 @@ import { store } from "../store"
 	display: flex;
 	flex-flow: column;
 	gap: 20px;
+}
+.add-note {
+	background: none;
+	color: inherit;
+	padding: 10px 10px 6px;
+	font: inherit;
+	cursor: pointer;
+	outline: inherit;
+	width: 100%;
+	border: 2px solid rgb(0 154 92 / 50%);
+	box-shadow: rgb(0 189 113 / 20%) 0 0 0 0;
+	border-radius: 10px;
+	transition: box-shadow 0.2s ease-in-out, border-color 0.2s ease-in-out;
+}
+.add-note:hover {
+	border: 2px solid rgb(0 154 92);
+	box-shadow: rgb(0 189 113 / 80%) 0 0 10px 0;
 }
 </style>
