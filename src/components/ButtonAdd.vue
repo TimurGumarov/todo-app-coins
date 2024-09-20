008a78ec2f28d5ce0e88518c227d8a69bf0b0bd5
Timur Gumarov <29134829+TimurGumarov@users.noreply.github.com>
2024-09-20 06:03:37 +0200

    Added ButtonAdd and CheckBox components

---
+++ src/components/ButtonAdd.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import IconAdd from "../assets/icon-add.vue"
+</script>
+
+<template>
+	<button class="button add" @click="$emit('click')">
+		<IconAdd class="icon" :style="{ fill: 'rgb(0 154 92)' }" />
+	</button>
+</template>
+
+<style>
+.button.add {
+	position: relative;
+	background: none;
+	color: inherit;
+	padding: 10px 10px 6px;
+	font: inherit;
+	cursor: pointer;
+	outline: inherit;
+	width: 100%;
+	border: 2px solid rgb(0 154 92 / 50%);
+	box-shadow: rgb(0 189 113 / 20%) 0 0 0 0;
+	border-radius: 10px;
+	transition: box-shadow 0.5s cubic-bezier(0.25, 1, 0.5, 1),
+		border-color 0.2s ease-in-out;
+}
+.button.add:hover {
+	border: 2px solid rgb(0 154 92);
+	box-shadow: rgb(0 189 113 / 80%) 0 0 10px 0;
+}
+.button.add .icon {
+	width: 20px;
+	height: 20px;
+}
+</style>
---
+++ src/components/CheckBox.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import IconCheck from "../assets/icon-check.vue"
+const modelChecked = defineModel<boolean>()
+</script>
+
+<template>
+	<label class="checkbox">
+		<input type="checkbox" v-model="modelChecked" />
+		<IconCheck class="checkmark" />
+	</label>
+</template>
+
+<style scoped>
+.checkbox {
+	display: block;
+	position: relative;
+	cursor: pointer;
+	width: 24px;
+	height: 24px;
+	border: 2px solid rgb(21 84 59 / 80%);
+	border-radius: 4px;
+	flex-shrink: 0;
+	-webkit-user-select: none;
+	-moz-user-select: none;
+	-ms-user-select: none;
+	user-select: none;
+}
+.checkbox input {
+	position: absolute;
+	opacity: 0;
+	cursor: pointer;
+	height: 0;
+	width: 0;
+}
+.checkmark {
+	position: absolute;
+	top: 0;
+	left: 0;
+	height: 24px;
+	width: 24px;
+	opacity: 0;
+	fill: rgb(0 189 113 / 80%);
+}
+.checkbox:hover {
+	border-color: rgb(0 189 113 / 80%);
+	box-shadow: rgb(0 189 113 / 20%) 0 0 10px 0;
+}
+.checkbox:has(input:checked) {
+	border-color: rgb(0 189 113 / 80%);
+}
+
+/* On mouse-over, add a grey background color */
+.checkbox:hover input ~ .checkmark {
+	opacity: 0.4;
+}
+
+/* When the checkbox is checked, add a blue background */
+.checkbox input:checked ~ .checkmark {
+	opacity: 1;
+}
+
+/* Create the checkmark/indicator (hidden when not checked) */
+.checkmark:after {
+	content: "";
+	position: absolute;
+	display: none;
+}
+</style>
