b4a626693d936a7694d4f0566e4885e29222069e
Timur Gumarov <29134829+TimurGumarov@users.noreply.github.com>
2024-09-20 05:09:49 +0200

    Added editable text previre, changed EditPage logic

---
+++ src/components/TextInputPreview.vue
@@ -0,0 +1,118 @@
+<script setup lang="ts">
+import { nextTick, PropType, ref, useTemplateRef } from "vue"
+import IconEdit from "../assets/icon-edit.vue"
+import IconCheck from "../assets/icon-check.vue"
+
+const modelText = defineModel<string>()
+const props = defineProps({
+	tag: {
+		type: String as PropType<"h1" | "p">,
+		required: false,
+		default: "p",
+	},
+})
+
+const isEditing = ref(false)
+const textarea = useTemplateRef("textareaRef")
+
+const enterEditing = async () => {
+	isEditing.value = true
+	await nextTick()
+	textarea.value?.focus()
+}
+const exitEditing = () => {
+	isEditing.value = false
+}
+</script>
+
+<template>
+	<div class="wrapper" v-if="!isEditing">
+		<h1
+			v-if="props.tag === 'h1'"
+			:class="$attrs.class"
+			@click="enterEditing"
+		>
+			<IconEdit class="icon edit" />{{ modelText }}
+		</h1>
+		<p v-if="props.tag === 'p'" :class="$attrs.class" @click="enterEditing">
+			<IconEdit class="icon edit small" />{{ modelText }}
+		</p>
+	</div>
+	<div class="wrapper" v-if="isEditing">
+		<textarea
+			v-model="modelText"
+			:class="$attrs.class"
+			ref="textareaRef"
+			@focusout="exitEditing"
+		></textarea>
+		<IconCheck class="icon save" :class="{ small: props.tag === 'p' }" />
+	</div>
+</template>
+
+<style scoped>
+.wrapper {
+	position: relative;
+}
+h1,
+p {
+	position: relative;
+	padding: 8px;
+	margin: 0;
+	border: 2px solid transparent;
+	border-radius: 10px;
+	white-space: pre-wrap;
+	transition: border-color 0.2s ease-in-out;
+	box-sizing: border-box;
+	cursor: text;
+}
+.icon.edit {
+	position: absolute;
+	right: 8px;
+	top: 8px;
+	width: 40px;
+	height: 40px;
+	fill: rgb(0 189 113 / 0%);
+	transition: fill 0.1s ease-in-out;
+}
+.icon.edit.small {
+	width: 20px;
+	height: 20px;
+}
+h1:hover,
+p:hover {
+	border: 2px solid rgb(0 189 113 / 40%);
+}
+h1:hover .icon,
+p:hover .icon {
+	fill: rgb(0 189 113 / 80%);
+}
+textarea {
+	display: block;
+	width: 100%;
+	padding: 8px;
+	margin: 0;
+	border: 2px solid rgb(0 189 113 / 80%);
+	border-radius: 10px;
+	font-family: inherit;
+	outline: none;
+	resize: none;
+	field-sizing: content;
+	box-shadow: rgb(0 189 113 / 10%) 0 0 40px 0;
+	box-sizing: border-box;
+}
+.icon.save {
+	position: absolute;
+	top: 8px;
+	right: -44px;
+	width: 40px;
+	height: 40px;
+	fill: rgb(0 189 113 / 80%);
+}
+.icon.save.small {
+	bottom: unset;
+	top: 10px;
+	right: -25px;
+	width: 20px;
+	height: 20px;
+}
+</style>
--- src/pages/EditPage.vue
+++ src/pages/EditPage.vue
@@ -1,20 +1,37 @@
 <script setup lang="ts">
 import { store } from "../store"
+import iconArrowLeft from "../assets/icon-arrow-left.vue"
+import TextInputPreview from "../components/TextInputPreview.vue"
 
 const props = defineProps({
 	id: Number,
 })
 
+let newNote = false
+if (props.id === -1) newNote = true
+
 const currentNote = store.notes.filter((note) => note.id === props.id)?.pop()
+const bigTitleMinLength = 25
+const titleIsBig =
+	currentNote && currentNote.title.length > bigTitleMinLength ? true : false
 </script>
 
 <template>
-	<div class="layout" v-if="currentNote">
-		<h1>{{ currentNote.title }}</h1>
-		<div class="note" @click="$emit('exitEditing')">
+	<div class="layout">
+		<button class="back" @click="$emit('exitEditing')">
+			<iconArrowLeft class="icon" :style="{ fill: 'white' }" />
+		</button>
+		<TextInputPreview
+			class="heading1"
+			:class="{ bigTitle: titleIsBig }"
+			v-if="currentNote"
+			v-model="currentNote.title"
+			:tag="'h1'"
+		/>
+		<div class="note" v-if="currentNote">
 			<ul v-if="currentNote.tasks?.length">
 				<li v-for="task of currentNote.tasks" :key="task.id">
-					{{ task.id }} {{ task.text }}
+					<TextInputPreview class="paragraph" v-model="task.text" />
 				</li>
 			</ul>
 		</div>
@@ -23,11 +40,44 @@ const currentNote = store.notes.filter((note) => note.id === props.id)?.pop()
 
 <style scoped>
 .layout {
+	position: relative;
 	display: flex;
 	flex-flow: column;
 	gap: 20px;
 }
+button.back {
+	position: absolute;
+	display: flex;
+	flex-flow: row;
+	align-items: center;
+	justify-content: left;
+	top: 20px;
+	left: 10px;
+	background: none;
+	color: inherit;
+	padding: 0;
+	font: inherit;
+	cursor: pointer;
+	outline: inherit;
+	width: 40px;
+	height: 40px;
+	border: none;
+	z-index: 1000;
+}
+button.back .icon {
+	width: 30px;
+	height: 30px;
+}
 .note {
+	display: flex;
+	flex-flow: column;
+	gap: 10px;
+}
+:deep(.bigTitle) {
+	text-align: left;
+	text-indent: 50px;
+}
+.note ul {
 	display: flex;
 	flex-flow: column;
 	gap: 10px;
